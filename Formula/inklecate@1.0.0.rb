1f3d48e0742e81540c606634c4f0b63ba65222d1
Frédéric Maquin <frederic@maquin.email>
2021-02-26 14:25:19 +0100

    Add inklecate 1.0.0

--- Formula/inklecate@1.0.0.rb
+++ Formula/inklecate@1.0.0.rb
@@ -1,4 +1,4 @@
-class Inklecate < Formula
+class InklecateAT100 < Formula
   inklecate_version = "1.0.0"
 
   desc "Compiler and runtime for inkle's ink"
@@ -8,6 +8,7 @@ class Inklecate < Formula
   sha256 "b6f4dd1f95c180637ce193dbb5fa6d59aeafe49a2121a05b7822e6cbbaa6931f"
 
   bottle :unneeded
+  keg_only :versioned_formula
 
   def install
     libexec.install Dir["*"]
